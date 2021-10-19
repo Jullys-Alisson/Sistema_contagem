f6cab286f218e2ff8372f5b1e9d318628b613455
Jullys Alisson <jullysalisson10@gmail.com>
2021-10-19 17:11:03 -0300

    Sistema de contagem 1.1

--- contagem.js
+++ contagem.js
@@ -6,30 +6,30 @@ console.log ("Jullys")
 console.log ("Jullys")
 console.log ("Jullys")*/
 
-for (let contador = 0; contador < lista_de_alunos.length; contador ++) {
-    //console.log (contador)
-
-    if (contador == 0) {
-        console.log (contador + ": Zero") //concatenação
-    } else if (contador % 2 == 1) {// % (resto) se a divisão interia por 2 for igual a 1 = Impar
-        console.log (`${contador}: Ímpar`) // Interpolação - Use `${variável}
-    } else {
-        console.log(`${contador}: Par`)
-    }
-
-}
-
-// let contador = 0
-
-// while (contador < lista_de_alunos.length) {
+// for (let contador = 0; contador < lista_de_alunos.length; contador ++) {
+//     //console.log (contador)
 
 //     if (contador == 0) {
-//         console.log(contador + ": Zero") //concatenação
-//     } else if (contador % 2 != 0) {// % (resto) se a divisão interia por 2 for igual a 1 = Impar
-//         console.log(`${contador}: Ímpar`) // Interpolação - Use `${variável}
+//         console.log (contador + ": Zero") //concatenação
+//     } else if (contador % 2 == 1) {// % (resto) se a divisão interia por 2 for igual a 1 = Impar
+//         console.log (`${contador}: Ímpar`) // Interpolação - Use `${variável}
 //     } else {
 //         console.log(`${contador}: Par`)
 //     }
-    
-//     contador++
+
 // }
+
+let contador = 0
+
+while (contador < lista_de_alunos.length) {
+
+    if (contador == 0) {
+        console.log(contador + ": Zero" + lista_de_alunos [contador]) //concatenação
+    } else if (contador % 2 != 0) {// % (resto) se a divisão interia por 2 for igual a 1 = Impar
+        console.log(`${contador}: Ímpar: ${lista_de_alunos[contador]}`) // Interpolação - Use `${variável}
+    } else {
+        console.log(`${contador}: Par: ${lista_de_alunos[contador]}`)
+    }
+    
+    contador++
+}
